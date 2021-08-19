9a68526297e9171fe96c5778a5d7b07777799558
Lucas Pinheiro <54106198+lppinheiro@users.noreply.github.com>
2021-08-19 16:41:49 -0300

    Add files via upload

---
+++ 2 - tag.js
@@ -0,0 +1,6 @@
+let nome = prompt("Digite seu nome:");
+let endereco  = prompt("Digite seu endereço:");
+let cep = prompt("Digite seu CEP:");
+let telefone = prompt("Digite seu telefone:");
+
+console.log(`Bem vindo ${nome}, como solicitado enviaremos seu pacote no entedeço: ${endereco} de CEP: ${cep}. Qualquer informação te enviaremos no  telefone: ${telefone} informado acima. Obrigado`);
